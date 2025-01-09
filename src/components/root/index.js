2f7964e05e4115ded3786d0a94de5f4834d9dca0
Catherina17 <kate.penkova2016@mail.ru>
2025-01-09 20:55:57 +0300

    styles

--- src/components/root/index.js
+++ src/components/root/index.js
@@ -1,22 +1,43 @@
 import React from 'react'
 import { NavLink, Outlet } from 'react-router-dom'
-import styles from './style.css'
+import styles from './style.module.css'
 
-<NavLink 
-    style={({ isActive }) => {
-        return {
-            color: isActive ? "yellow" : '',
-        };
-    }}
-    className={({ isActive }) => {
-        return isActive ? styles.active : '';
-    }} 
-/>
+// {/* <NavLink 
+//     style={({ isActive }) => {
+//         return {
+//             color: isActive ? "yellow" : '',
+//         };
+//     }}
+//     className={({ isActive }) => {
+//         return isActive ? styles.active : '';
+//     }} 
+// /> */}
 
 export const Root = () => <>
-    <NavLink to='/'>Главная</NavLink>
-    <NavLink to='posts'>Посты</NavLink>
-    <NavLink to='blog'>Блог</NavLink>
+    <NavLink 
+        to='/' 
+        className={({ isActive }) => {
+            return isActive ? styles.home : '';
+        }}
+    >
+        Главная
+    </NavLink>
+    <NavLink 
+        to='posts' 
+        className={({ isActive }) => {
+            return isActive ? styles.post : '';
+        }}
+    >
+        Посты
+    </NavLink>
+    <NavLink 
+        to='blog' 
+        className={({ isActive }) => {
+            return isActive ? styles.blog : '';
+        }}
+    >
+        Блог
+    </NavLink>
     <br />
     <br />
     <br />
