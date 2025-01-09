708e4e78515d35e652a5c55141783892e6af07d1
Catherina17 <kate.penkova2016@mail.ru>
2025-01-09 20:57:32 +0300

    styles module

--- src/components/root/index.js
+++ src/components/root/index.js
@@ -2,17 +2,6 @@ import React from 'react'
 import { NavLink, Outlet } from 'react-router-dom'
 import styles from './style.module.css'
 
-// {/* <NavLink 
-//     style={({ isActive }) => {
-//         return {
-//             color: isActive ? "yellow" : '',
-//         };
-//     }}
-//     className={({ isActive }) => {
-//         return isActive ? styles.active : '';
-//     }} 
-// /> */}
-
 export const Root = () => <>
     <NavLink 
         to='/' 
